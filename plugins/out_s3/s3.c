b3512155d04720011b98fe47617d1cc3560909b1
Brent-Crawley-GGL <brent.crawley@gallagher.com>
2025-07-18 12:28:07 +1200

    s3: Update the TLS interface for OpenSSL Provider support
    Signed-off-by: Brent-Crawley-GGL <brent.crawley@gallagher.com>

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -869,7 +869,8 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                          ins->tls_ca_file,
                                          ins->tls_crt_file,
                                          ins->tls_key_file,
-                                         ins->tls_key_passwd);
+                                         ins->tls_key_passwd, 
+                                         ins->tls_provider_query);
         if (!ctx->client_tls) {
             flb_plg_error(ctx->ins, "Failed to create tls context");
             return -1;
@@ -885,7 +886,8 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                        ins->tls_ca_file,
                                        ins->tls_crt_file,
                                        ins->tls_key_file,
-                                       ins->tls_key_passwd);
+                                       ins->tls_key_passwd, 
+                                       ins->tls_provider_query);
     if (!ctx->provider_tls) {
         flb_errno();
         return -1;
@@ -919,7 +921,8 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                                ins->tls_ca_file,
                                                ins->tls_crt_file,
                                                ins->tls_key_file,
-                                               ins->tls_key_passwd);
+                                               ins->tls_key_passwd, 
+                                               ins->tls_provider_query);
 
         if (!ctx->sts_provider_tls) {
             flb_errno();
