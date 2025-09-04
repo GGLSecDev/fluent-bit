93f4307c02ac69162e0c1f09bcc855ecf5224255
Craig Robb <craig.robb@gallagher.com>
2025-09-05 07:20:27 +1200

    azure: updated for tls_verifier argument in tls_context_create function
    Signed-off-by: Craig Robb <craig.robb@gallagher.com>

--- plugins/out_azure_blob/azure_blob_conf.c
+++ plugins/out_azure_blob/azure_blob_conf.c
@@ -376,6 +376,7 @@ static int flb_azure_blob_apply_remote_configuration(struct flb_azure_blob *cont
                                  NULL,
                                  NULL,
                                  NULL,
+                                 NULL,
                                  NULL);
 
     if (tls_context == NULL) {
