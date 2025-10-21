26885ad7fd3d7e57fe29b4f22c1db9048333338a
Brent-Crawley-GGL <brent.crawley@gallagher.com>
2025-10-22 07:42:50 +1300

    output: updated TLS Verifier name to Network Verifier. Added a pointer to Network Verifier instance be able to notify of network errors
    Signed-off-by: Brent Crawley <brent.crawley@gallagher.com>

--- include/fluent-bit/flb_output.h
+++ include/fluent-bit/flb_output.h
@@ -377,8 +377,9 @@ struct flb_output_instance {
     char *tls_win_thumbprints;               /* CertStore Thumbprints (Windows) */
 # endif
     char *tls_provider_query;            /* OpenSSL Provider Query */
-    char *tls_verifier;                  /* TLS Verifier plugin alias */
 #endif
+    char* network_verifier;           /* Connection Verifier alias */
+    struct flb_network_verifier_instance* verifier_ins;
 
     /*
      * network info:
