0ae5e03332d35d0d4512e61840eaa4fa1d9588e0
Brent-Crawley-GGL <brent.crawley@gallagher.com>
2025-10-22 07:19:14 +1300

    firehose: updated TLS Verifier name to Network Verifier
    Signed-off-by: Brent Crawley <brent.crawley@gallagher.com>

--- plugins/out_kinesis_firehose/firehose.c
+++ plugins/out_kinesis_firehose/firehose.c
@@ -59,7 +59,7 @@ static int cb_firehose_init(struct flb_output_instance *ins,
     char *session_name = NULL;
     struct flb_firehose *ctx = NULL;
     int ret;
-    const struct flb_tls_verifier_instance *tls_ins = NULL;
+    const struct flb_network_verifier_instance *conn_ins = NULL;
     (void) config;
     (void) data;
 
@@ -174,7 +174,7 @@ static int cb_firehose_init(struct flb_output_instance *ins,
         ctx->role_arn = tmp;
     }
 
-    tls_ins = find_tls_verifier_instance(config, ins->tls_verifier);
+    conn_ins = find_network_verifier_instance(config, ins->network_verifier);
 
     /* one tls instance for provider, one for cw client */
     ctx->cred_tls = flb_tls_create(FLB_TLS_CLIENT_MODE,
@@ -187,7 +187,7 @@ static int cb_firehose_init(struct flb_output_instance *ins,
                                    ins->tls_key_file,
                                    ins->tls_key_passwd,
                                    ins->tls_provider_query,
-                                   tls_ins);
+                                   conn_ins);
 
     if (!ctx->cred_tls) {
         flb_plg_error(ctx->ins, "Failed to create tls context");
@@ -204,7 +204,7 @@ static int cb_firehose_init(struct flb_output_instance *ins,
                                      ins->tls_key_file,
                                      ins->tls_key_passwd,
                                      ins->tls_provider_query,
-                                     tls_ins);
+                                     conn_ins);
     if (!ctx->client_tls) {
         flb_plg_error(ctx->ins, "Failed to create tls context");
         goto error;
@@ -242,7 +242,7 @@ static int cb_firehose_init(struct flb_output_instance *ins,
                                       ins->tls_key_file,
                                       ins->tls_key_passwd,
                                       ins->tls_provider_query,
-                                      tls_ins);
+                                      conn_ins);
         if (!ctx->sts_tls) {
             flb_errno();
             goto error;
