acb216bae5660c7962c5deb7a8b1c6e530485eb8
Brent-Crawley-GGL <brent.crawley@gallagher.com>
2025-10-22 07:20:08 +1300

    s3: updated TLS Verifier name to Network Verifier
    Signed-off-by: Brent Crawley <brent.crawley@gallagher.com>

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -526,7 +526,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
     struct flb_split_entry *tok;
     struct mk_list *split;
     int list_size;
-    const struct flb_tls_verifier_instance *tls_ins = NULL;
+    const struct flb_network_verifier_instance *conn_ins = NULL;
 
     ctx = flb_calloc(1, sizeof(struct flb_s3));
     if (!ctx) {
@@ -777,7 +777,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
         ctx->storage_class = (char *) tmp;
     }
 
-    tls_ins = find_tls_verifier_instance(config, ins->tls_verifier);
+    conn_ins = find_network_verifier_instance(config, ins->network_verifier);
     if (ctx->insecure == FLB_FALSE) {
         ctx->client_tls = flb_tls_create(FLB_TLS_CLIENT_MODE,
                                          ins->tls_verify,
@@ -789,7 +789,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                          ins->tls_key_file,
                                          ins->tls_key_passwd,
                                          ins->tls_provider_query,
-                                         tls_ins);
+                                         conn_ins);
         if (!ctx->client_tls) {
             flb_plg_error(ctx->ins, "Failed to create tls context");
             return -1;
@@ -807,7 +807,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                        ins->tls_key_file,
                                        ins->tls_key_passwd,
                                        ins->tls_provider_query,
-                                       tls_ins);
+                                       conn_ins);
     if (!ctx->provider_tls) {
         flb_errno();
         return -1;
@@ -843,7 +843,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                                ins->tls_key_file,
                                                ins->tls_key_passwd,
                                                ins->tls_provider_query,
-                                               tls_ins);
+                                               conn_ins);
 
         if (!ctx->sts_provider_tls) {
             flb_errno();
