2bfedb5cb9cb3a606aed934fa371bd4796308842
Craig Robb <craig.robb@gallagher.com>
2025-11-06 14:05:04 +1300

    s3: pass through base instance type network verifier instead of refinding it
    Signed-off-by: Craig Robb <craig.robb@gallagher.com>

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -526,7 +526,6 @@ static int cb_s3_init(struct flb_output_instance *ins,
     struct flb_split_entry *tok;
     struct mk_list *split;
     int list_size;
-    const struct flb_network_verifier_instance *conn_ins = NULL;
 
     ctx = flb_calloc(1, sizeof(struct flb_s3));
     if (!ctx) {
@@ -777,7 +776,6 @@ static int cb_s3_init(struct flb_output_instance *ins,
         ctx->storage_class = (char *) tmp;
     }
 
-    conn_ins = find_network_verifier_instance(config, ins->network_verifier);
     if (ctx->insecure == FLB_FALSE) {
         ctx->client_tls = flb_tls_create(FLB_TLS_CLIENT_MODE,
                                          ins->tls_verify,
@@ -789,7 +787,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                          ins->tls_key_file,
                                          ins->tls_key_passwd,
                                          ins->tls_provider_query,
-                                         conn_ins);
+                                         ins->verifier_ins);
         if (!ctx->client_tls) {
             flb_plg_error(ctx->ins, "Failed to create tls context");
             return -1;
@@ -807,7 +805,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                        ins->tls_key_file,
                                        ins->tls_key_passwd,
                                        ins->tls_provider_query,
-                                       conn_ins);
+                                       ins->verifier_ins);
     if (!ctx->provider_tls) {
         flb_errno();
         return -1;
@@ -843,7 +841,7 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                                ins->tls_key_file,
                                                ins->tls_key_passwd,
                                                ins->tls_provider_query,
-                                               conn_ins);
+                                               ins->verifier_ins);
 
         if (!ctx->sts_provider_tls) {
             flb_errno();
