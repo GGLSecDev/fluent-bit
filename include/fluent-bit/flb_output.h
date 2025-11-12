ed04b5eae247dbbc812c08a54a78a3d6bef25018
Craig Robb <craig.robb@gallagher.com>
2025-11-13 10:16:32 +1300

    output: included network_verifier header file to avoid unknown type errors
    Signed-off-by: Craig Robb <craig.robb@gallagher.com>

--- include/fluent-bit/flb_output.h
+++ include/fluent-bit/flb_output.h
@@ -48,6 +48,7 @@
 #include <fluent-bit/flb_upstream_ha.h>
 #include <fluent-bit/flb_event.h>
 #include <fluent-bit/flb_processor.h>
+#include <fluent-bit/flb_network_verifier.h>
 
 #include <cmetrics/cmetrics.h>
 #include <cmetrics/cmt_gauge.h>
